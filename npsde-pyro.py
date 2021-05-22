677d79ba9af2a2d15a7164c5a154f43165687f23
Kuan <kuanjh123@gmail.com>
2021-05-22 13:26:14 -0500

    added plotting

--- npsde-pyro.py
+++ npsde-pyro.py
@@ -2,8 +2,12 @@ import pandas as pd
 import numpy as np
 import pyro
 import torch
+import os 
 import pyro.distributions as dist
 import torch.distributions.constraints as constraints
+import matplotlib.pyplot as plt 
+import matplotlib as mpl 
+
 from pyro.infer import SVI, Trace_ELBO
 
 class Kernel:
@@ -38,6 +42,9 @@ class NPSDE():
     '''
     Implementation of Yildiz NPSDE algorithm.
     '''
+
+    hyperparameters = ['vars','sf_f','sf_g','ell_f','ell_g','Z','fix_sf','fix_ell','fix_Z','delta_t','jitter']
+
     def __init__(self,vars,sf_f,sf_g,ell_f,ell_g,Z,fix_sf,fix_ell,fix_Z,delta_t,jitter):
 
         self.vars = vars #list of variable names to be estimated. (ex) ['PCA0','PCA1']
@@ -50,6 +57,11 @@ class NPSDE():
         self.ell_g = ell_g if fix_ell else pyro.param('ell_g', ell_g)
         self.Z = Z if fix_Z else pyro.param('Z', Z)
         self.Zg = self.Z #Inducing location for drift and diffusion are set same.
+
+        ##For save_model
+        self.fix_sf = fix_sf 
+        self.fix_ell = fix_ell 
+        self.fix_Z = fix_Z 
 
         ##Define kernels
         self.kernel_f = Kernel(self.sf_f, self.ell_f)
@@ -96,13 +108,12 @@ class NPSDE():
         U = torch.mm(Lz,U_whitened)
         return U
 
-    def model(self,df,balanced_df):
+    def model(self,X):
         '''
         NPSDE model.
-        df is a Pandas DataFrame whose keys include variables in vars (such as ['PC0','PC1']) plus "time".
-        balanced_df is the same data as a balanced panel, where all the time steps are filled (potentially with missing values).
+        X : 2D array, first column for timestamps, the rest for components of timeseries 
         '''
-        t_max = df['time'].max()
+        t_max = X[:,0].max()
         t_grid = np.arange(1,t_max+1)
 
         ##Inducing vectors, which are the main parameters to be estimated
@@ -113,27 +124,28 @@ class NPSDE():
         #Ug = pyro.sample('Ug', dist.HalfNormal(torch.ones([n_grid**2,2]) ).to_event(1).to_event(1) )#,constraint=constraints.positive #Prior should be matched to Yildiz?
 
         ##Euler-Maruyama sampling
-        X = torch.tensor(df[df.time==0][self.vars].values.astype(np.float32))
-        for t in np.arange(self.delta_t, t_max+self.delta_t, self.delta_t):
-            f,g = self.calc_drift_diffusion(X, U, Ug, self.Z, self.Zg, self.kernel_f, self.kernel_g)
-            X = pyro.sample('Xseq_{}'.format(t), dist.Normal(X + f * self.delta_t, g * torch.sqrt(torch.tensor([self.delta_t])) ).to_event(1).to_event(1)   )#Needs to be MultiVariate and iterate over sample to allow covariance.
+        Xt = torch.tensor(X[X[:,0]==1][:, 1:])
+        timestamps = np.arange(self.delta_t, t_max+self.delta_t, self.delta_t) 
+        for i, t in enumerate(timestamps):
+            f,g = self.calc_drift_diffusion(Xt, U, Ug, self.Z, self.Zg, self.kernel_f, self.kernel_g)
+            Xt = pyro.sample('Xseq_{}'.format(i), dist.Normal(Xt + f * self.delta_t, g * torch.sqrt(torch.tensor([self.delta_t])) ).to_event(1).to_event(1)   )#Needs to be MultiVariate and iterate over sample to allow covariance.
             ##For t in the observed time step, find the observed variables and condition on the data.
             if t in t_grid:
-                idx = (~balanced_df[balanced_df['time']==t][self.vars].isna()).values
+                idx = (~np.isnan(X[X[:,0]==t][:, 1:]))
                 if np.sum(idx)!=0:
-                    df_t  = balanced_df[balanced_df['time']==t][self.vars]
-                    X_obs = torch.tensor( df_t.dropna().values.flatten().astype(np.float32) )
-                    X_sample = X[[idx]]
+                    df_t  = X[X[:,0]==t][:, 1:]
+                    Xt_obs = torch.tensor( df_t[~np.isnan(df_t)].flatten() )
+                    Xt_sample = Xt[[idx]]
                     ##Note that this flattens all the observed variable into a flat vector.
-                    pyro.sample('Xobs_{}'.format(t), dist.Normal(X_sample, 1  ).to_event(1), obs=X_obs )
+                    pyro.sample('Xobs_{}'.format(i), dist.Normal(Xt_sample, 1  ).to_event(1), obs=Xt_obs )
 
 
-    def guide_map(self,df,balanced_df):
+    def guide_map(self,X):
         '''
         The "guide" for MAP estimation of NPSDE model.
         '''
 
-        t_max = df['time'].max()
+        t_max = X[:,0].max()
         ##MAP estimate of parameters
         U_map = pyro.param('U_map', torch.zeros([self.n_grid,self.n_vars]) )
         #Ug_map = pyro.param('Ug_map', torch.ones([n_grid**2,2])*torch.sqrt(torch.tensor(2) )/torch.tensor(np.pi)  )#, constraint=constraints.positive
@@ -142,24 +154,179 @@ class NPSDE():
         Ug = pyro.sample("Ug", dist.Delta(Ug_map).to_event(1).to_event(1))
 
         ##Euler-Maruyama sampling
-        X = torch.tensor(df[df.time==0][self.vars].values.astype(np.float32))
-        for t in np.arange(self.delta_t, t_max+self.delta_t, self.delta_t):
-            f,g = self.calc_drift_diffusion(X, U, Ug, self.Z, self.Zg, self.kernel_f, self.kernel_g)
-            X = pyro.sample('Xseq_{}'.format(t), dist.Normal(X + f * self.delta_t, g * torch.sqrt(torch.tensor([self.delta_t])) ).to_event(1).to_event(1)  )#Needs to be MultiVariate and iterate over sample to allow covariance.
+        timestamps = np.arange(self.delta_t, t_max+self.delta_t, self.delta_t) 
+
+        Xt = torch.tensor(X[X[:,0]==1][:, 1:]) # N x D 
+
+        # Xs = torch.zeros([X.shape[0], len(timestamps), X.shape[1]]) # N x t x D 
+        for i,t in enumerate(timestamps):
+            f,g = self.calc_drift_diffusion(Xt, U, Ug, self.Z, self.Zg, self.kernel_f, self.kernel_g)
+            Xt = pyro.sample('Xseq_{}'.format(i), dist.Normal(Xt + f * self.delta_t, g * torch.sqrt(torch.tensor([self.delta_t])) ).to_event(1).to_event(1)  )#Needs to be MultiVariate and iterate over sample to allow covariance.
+        #     Xs[:, i, :] = X 
+
+        # return Xs 
 
 
-    def train(self, df,balanced_df, n_steps=1001, lr=0.01):
-        pyro.clear_param_store()
+    def train(self, X, n_steps=1001, lr=0.01, Nw=50):
+        # pyro.clear_param_store()
         adam = pyro.optim.Adam({"lr": lr})
-        self.svi = SVI(self.model, self.guide_map, adam, loss=Trace_ELBO())
+        self.svi = SVI(self.model, self.guide_map, adam, loss=Trace_ELBO(num_particles=Nw))
         for step in range(n_steps):
-            loss = self.svi.step(df,balanced_df)
+            loss = self.svi.step(X)
             if step % 50 == 0:
                 print('[iter {}]  loss: {:.4f}'.format(step, loss))
 
+    def mc_samples(self, X, Nw=1):
+        t_max = X[:,0].max()
+        timeseries_count = len(X[X[:,0]==1])
+        timestamps = np.arange(self.delta_t, t_max+self.delta_t, self.delta_t) 
+        return_sites = [] 
+        for i,t in enumerate(timestamps):
+            if t in X[:,0]:
+                return_sites += ['Xseq_{}'.format(i)]
+        predictive = pyro.infer.Predictive(self.model, guide=self.guide_map, num_samples=Nw, return_sites=return_sites)
+        Xs = np.zeros((Nw, timeseries_count, X.shape[1]-1, len(return_sites) + 1)) # Nw x N x D x T 
+        for i in range(Nw):
+            Xs[i, :, :, 0] = X[X[:,0]==1,1:]
+        pred = predictive.forward(X)
+        for i, time in enumerate(return_sites):
+            Xs[:, :, :,  i+1] = pred[time].detach()
+
+        
+
+        return Xs 
+
+    def save_model(self, path):
+        filename = os.path.basename(path)
+        pyro.get_param_store().save(os.path.join(os.path.dirname(path), os.path.splitext(filename)[0] + '_params' + os.path.splitext(filename)[1]))
+        constant_hyperparameters = list(set(NPSDE.hyperparameters).difference(set(pyro.get_param_store().get_all_param_names()))) # Get list of constant hyperparameters 
+        torch.save({
+            'constants' : { param : getattr(self, param) for param in constant_hyperparameters}
+        }, path) 
+
+    @staticmethod
+    def load_model(path):
+        filename = os.path.basename(path)
+
+        pyro.get_param_store().clear()
+        pyro.get_param_store().load(os.path.join(os.path.dirname(path), os.path.splitext(filename)[0] + '_params' + os.path.splitext(filename)[1]))
+        metadata = torch.load(path)
+
+        constr_args = [
+            metadata['constants'][param] if param in metadata['constants'] else pyro.get_param_store().get_param(param) for param in NPSDE.hyperparameters
+        ]
+
+        # Create new npsde object and attach fields to params 
+        return NPSDE(*constr_args) 
+
+
+    def plot_model(self, X, prefix="",Nw=1):
+        mpl.rc('text', usetex=True)
+
+        t_max = X[:, 0].max()
+        t_grid = np.arange(1,t_max+1)
+
+        # X0 = torch.tensor(df[df.time==0][self.vars].values.astype(np.float32))
+
+        with torch.no_grad():
+            Y = self.mc_samples(X, Nw=1) # Nw x N x D x T 
+            Z = self.Z.detach() 
+            Zg = self.Zg.detach() 
+            U = pyro.get_param_store().get_param('U_map').detach() 
+            Ug = pyro.get_param_store().get_param('Ug_map').detach() 
+
+        complete_data = X
+        complete_data = complete_data[~np.isnan(complete_data).any(axis=1), :]
+        breakoff = list(np.where(complete_data[:, 0] == 1)[0])
+        breakoff += [len(complete_data)]
+        X_timeseries = [complete_data[breakoff[i] : breakoff[i+1], 1:] for i in range(len(breakoff) - 1)]
+        plt.figure(1,figsize=(20,12))
+        gs = mpl.gridspec.GridSpec(1, 1)
+        ax1 = plt.subplot(gs[0,0])
+        for j in range(len(X_timeseries)):
+            for i in range(Y.shape[0]):
+                pathh, = ax1.plot(Y[i,j,0,:],Y[i,j,1,:],'b-',linewidth=0.5,label='samples')
+            dh, = ax1.plot(X_timeseries[j][:,0],X_timeseries[j][:,1],'-ro',markersize=4,linewidth=0.3,label='data points')
+            
+        
+        
+        ilh = ax1.scatter(Z[:,0],Z[:,1],100, facecolors='none', edgecolors='k',label='inducing locations')
+        ivh = ax1.quiver(Z[:,0],Z[:,1],U[:,0],U[:,1],units='height',width=0.006,color='k',label='inducing vectors')
+        ax1.set_xlabel('PC1', fontsize=30)
+        ax1.set_ylabel('PC2', fontsize=30)
+        # Commented out because it is causing image size issue 
+        ax1.legend(handles=[pathh,ilh,ivh,dh],loc=2) 
+        ax1.set_title('Vector Field',fontsize=30)
+        plt.savefig('%sdrift_sde.png' % prefix, dpi=400)
+        # plt.show()
+
+
+        # flattened_Y = np.asarray([Y[i][j] for i in range(len(Y)) for j in range(len(Y[i]))])
+        extents = [Z[:,0].min(), Z[:,0].max(), Z[:,1].min(), Z[:,1].max()]
+
+        
+        W = 50
+        # Fixed boundaries
+        xv = np.linspace(extents[0], extents[1], W)
+        yv = np.linspace(extents[2], extents[3], W)
+        xvv,yvv = np.meshgrid(xv,yv, indexing='ij')
+
+
+        Zs = np.array([xvv.T.flatten(),yvv.T.flatten()], dtype=np.float32).T
+
+        f,g = self.calc_drift_diffusion(Zs, U, Ug, self.Z, self.Zg, self.kernel_f, self.kernel_g)
+
+        # f = self.unwhiten_U(f, Zs, self.kernel_f)
+        # g = self.unwhiten_U(g, Zs, self.kernel_g)
+
+        Us = f.detach().numpy()
+        Ugs = g.detach().numpy()
+
+        fig = plt.figure(2, figsize=(15,12))
+        gs = mpl.gridspec.GridSpec(nrows=1, ncols=1)
+        ax1 = plt.subplot(gs[0,0])
+        Uc = np.sqrt(Us[:,0] ** 2 + Us[:,1] ** 2)
+        strm = ax1.streamplot(np.unique(list(Zs[:,0])), np.unique(list(Zs[:,1])), Us[:,0].reshape(W,W), Us[:,1].reshape(W,W), color=Uc.reshape(W,W), cmap='autumn')
+        ax1.set_title('Drift Stream')
+        fig.colorbar(strm.lines)
+        plt.savefig('%sdrift_stream.png' % prefix, dpi=200)
+
+        fig = plt.figure(2, figsize=(15,12))
+        gs = mpl.gridspec.GridSpec(nrows=1, ncols=20)
+        ax1 = plt.subplot(gs[0,0:19])
+        ax2 = plt.subplot(gs[0,19])
+
+        Ugs = Ugs.reshape(W, W, 2) 
+        mag = (Ugs[:,:,0] ** 2 + Ugs[:,:,1] ** 2) ** 0.5
+        x_max, y_max, mag_max, mag_min = Ugs[:,:,0].max(), Ugs[:,:,1].max() , mag.max(), mag.min()
+        x_delta, y_delta = (extents[1] - extents[0]) / W, (extents[3] - extents[2]) / W
+
+        Zs_grid = Zs.reshape(W, W, 2)
+        ellipses = [] 
+        cmap = mpl.cm.get_cmap('viridis')
+        for r in range(W):
+            for c in range(W):
+                ellipses += [ax1.add_patch(mpl.patches.Ellipse(Zs_grid[r, c], Ugs[r,c,0]*x_delta/x_max, Ugs[r,c,1]*y_delta/y_max, color=cmap(((Ugs[r,c,0] ** 2 + Ugs[r,c,1] ** 2) ** 0.5 - mag_min) / (mag_max - mag_min))))]
+
+        # Display locations and scales of diffusion inducing points 
+        # diff_Z = Zg
+        # diff_U = Ug
+        # diff_U = diff_U / np.max(diff_U) * (100 ** 2)
+        # ax1.scatter(diff_Z[:,0], diff_Z[:,1], s=diff_U, facecolors='none', edgecolors='white')
+        ax1.set_title('estimated diffusion')
+        ax1.set_xlabel('$PC_1$ x%.3f' % (x_max / x_delta), fontsize=12)
+        ax1.set_ylabel('$PC_2$ x%.3f' % (y_max / y_delta), fontsize=12)
+        ax1.set_xlim(extents[0], extents[1])
+        ax1.set_ylim(extents[2], extents[3])
+        mpl.colorbar.ColorbarBase(ax2, cmap = cmap, norm = mpl.colors.Normalize(vmin=mag_min, vmax=mag_max), orientation='vertical')
+        plt.savefig('%sdiff.png' % prefix, dpi=200)
+
+       
+
 if __name__ == '__main__':
     df = pd.read_csv('data/seshat/Seshat_old_pca.csv')
-    df['time'] = df[['NGA','Time']].groupby('NGA').transform(lambda x: (x - x.min()) / 100.)['Time']
+    components =['PCA0','PCA1']
+    df['time'] = df[['NGA','Time']].groupby('NGA').transform(lambda x: (x - x.min()) / 100. + 1)['Time']
     t_max = df['time'].max()
     t_grid = np.arange(1,t_max+1)
 
@@ -168,11 +335,21 @@ if __name__ == '__main__':
     balanced_df = pd.DataFrame( {'time':np.tile(t_grid, len(unit_list)), 'NGA':nga_grid} )
     balanced_df = balanced_df.merge(df,how='left')
 
-    pyro.clear_param_store()
+    X = balanced_df[['time'] + components].to_numpy(dtype=np.float32)
 
-    Zx_, Zy_ = np.meshgrid( np.linspace(df['PCA0'].min(), df['PCA0'].max(),5), np.linspace(df['PCA1'].min(), df['PCA1'].max(),5) )
-    Z = torch.tensor( np.c_[Zx_.flatten(), Zy_.flatten()].astype(np.float32) )
+    # pyro.clear_param_store()
 
-    npsde = NPSDE(vars=['PCA0','PCA1'],sf_f=torch.tensor(1,dtype=torch.float32),sf_g=torch.tensor(1,dtype=torch.float32),ell_f=torch.tensor([1,1],dtype=torch.float32),ell_g=torch.tensor([1,1],dtype=torch.float32),Z=Z,fix_sf=False,fix_ell=True,fix_Z=False,delta_t=.1,jitter=1e-6)
+    # Zx_, Zy_ = np.meshgrid( np.linspace(df['PCA0'].min(), df['PCA0'].max(),5), np.linspace(df['PCA1'].min(), df['PCA1'].max(),5) )
+    # Z = torch.tensor( np.c_[Zx_.flatten(), Zy_.flatten()].astype(np.float32) )
 
-    npsde.train(df,balanced_df)
+    # npsde = NPSDE(vars=components,sf_f=torch.tensor(1,dtype=torch.float32),sf_g=torch.tensor(1,dtype=torch.float32),ell_f=torch.tensor([1,1],dtype=torch.float32),ell_g=torch.tensor([1,1],dtype=torch.float32),Z=Z,fix_sf=False,fix_ell=True,fix_Z=False,delta_t=.1,jitter=1e-6)
+
+    # npsde.train(X, n_steps=3, Nw=1)
+
+    # npsde.save_model('model1.pt')
+
+    npsde = NPSDE.load_model('model1.pt')
+
+    npsde.plot_model(X, 'test')
+
+
